a6b818ace83c111cdf61e76f189b34a0bf592250
Paul Savage <paulsavage@Pauls-iMac.local>
2024-05-28 12:12:05 +0100

    adds solution to task 2 - GET /api/topics

---
+++ errors/index.js
@@ -0,0 +1,19 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  next(err);
+};
