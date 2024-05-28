559147a542abedbc74ea5a5644970611c9f99ac1
Paul Savage <paulsavage@Pauls-iMac.local>
2024-05-28 14:37:52 +0100

    adds solution to task 4 - get /api/articles/:article_id

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const {
 
 const express = require("express");
 
-const { getTopics } = require("./controllers/topics.controllers");
 const { getApis } = require("./controllers/apis.controllers");
+const { getTopics } = require("./controllers/topics.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 
 const app = express();
 
-app.get("/api/topics", getTopics);
 app.get("/api", getApis);
+
+app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
