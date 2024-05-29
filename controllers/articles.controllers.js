bca7ddf004cc7c1b291592d150295bcb0b087a57
Paul Savage <paulsavage@Pauls-iMac.local>
2024-05-29 13:12:49 +0100

    adds solution to task 7 - post-api-articles-article_id-comments

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -2,6 +2,7 @@ const {
   fetchArticleById,
   fetchArticles,
   fetchCommentsByArticleId,
+  storeCommentsByArticleId,
   checkExists,
 } = require("../models/articles.models.js");
 
@@ -30,6 +31,25 @@ exports.getCommentsByArticleId = (req, res, next) => {
     .then((results) => {
       const comments = results[0];
       res.status(200).send({ comments });
+    })
+    .catch(next);
+};
+
+exports.postCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  const data = req.body;
+  const { username } = data;
+
+  Promise.all([
+    checkExists("users", "username", username),
+    checkExists("articles", "article_id", article_id),
+  ])
+    .then(() => {
+      return storeCommentsByArticleId(article_id, data);
+    })
+    .then((rows) => {
+      const comment = rows[0];
+      res.status(201).send({ comment });
     })
     .catch(next);
 };
