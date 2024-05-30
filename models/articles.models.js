86afa45bf923d1bd5024c7b66550d66f38f39a66
Paul Savage <paulsavage@Pauls-iMac.local>
2024-05-30 11:17:28 +0100

    adds solution to task 12 - get-api-articles-article_id-comment_count

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,24 @@ const format = require("pg-format");
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .query(
+      `SELECT
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.body,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.article_id)::INT AS comment_count
+       FROM articles
+       LEFT JOIN comments
+       ON articles.article_id = comments.article_id
+       WHERE articles.article_id = $1
+       GROUP BY articles.article_id;`,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not Found" });
