9265968bece8143dfeed57d01441bd71fc7a3569
Paul Savage <paulsavage@Pauls-iMac.local>
2024-06-01 09:00:24 +0100

    adds solution to task 17 - get-api-users-username

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -382,3 +382,27 @@ describe("/api/users", () => {
       });
   });
 });
+
+describe("/api/users/:username", () => {
+  test("GET:200 sends the specified user to the client", () => {
+    return request(app)
+      .get("/api/users/butter_bridge")
+      .expect(200)
+      .then(({ body }) => {
+        const { user } = body;
+        expect(user.username).toBe("butter_bridge");
+        expect(user.name).toBe("jonny");
+        expect(user.avatar_url).toBe(
+          "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg"
+        );
+      });
+  });
+  test("GET:404 sends an appropriate status and error message when given a valid but non-existent username", () => {
+    return request(app)
+      .get("/api/users/paul")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
+      });
+  });
+});
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,18 @@
-const { fetchUsers } = require("../models/users.models.js");
+const { fetchUsers, fetchUserByName } = require("../models/users.models.js");
 
 exports.getUsers = (req, res, next) => {
   fetchUsers()
     .then((users) => {
       res.status(200).send({ users });
+    })
+    .catch(next);
+};
+
+exports.getUserByName = (req, res, next) => {
+  const { username } = req.params;
+  return fetchUserByName(username)
+    .then((user) => {
+      res.status(200).send({ user });
     })
     .catch(next);
 };
