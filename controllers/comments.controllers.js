39b62edb34ec4742d4b82bcd340647e5880c86e2
Paul Savage <paulsavage@Pauls-iMac.local>
2024-06-01 09:35:35 +0100

    adds solution to task 18 - patch-api-comments-comment_id

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,10 +1,26 @@
-const { removeCommentById } = require("../models/comments.models.js");
+const {
+  removeCommentById,
+  updateCommentById,
+} = require("../models/comments.models.js");
 
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
   removeCommentById(comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch(next);
+};
+
+exports.patchCommentById = (req, res, next) => {
+  const { comment_id } = req.params;
+  const data = req.body;
+  const { inc_votes } = data;
+
+  updateCommentById(comment_id, inc_votes)
+    .then((rows) => {
+      const comment = rows[0];
+      res.status(200).send({ comment });
     })
     .catch(next);
 };
